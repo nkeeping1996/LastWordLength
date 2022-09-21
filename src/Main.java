37fb5de65f14e3b10005c23891b6ff65508598da
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-21 09:25:46 -0400

    Final submission

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String s = "Pour one ounce of broth into the stew please";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        return s.split(" ")[s.split(" ").length-1].length();
     }
 }
